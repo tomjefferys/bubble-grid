b3c6dbca1cdb4f8cfcd5efce20e46b3c4b71ada9
Tom Jefferys <tomjefferys@gmail.com>
2025-03-25 19:15:30 +0000

    Make assetPrefix conditional

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   /* config options here */
   reactStrictMode: true,
-  assetPrefix: "/bubbles/",
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/bubbles/' : '',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
