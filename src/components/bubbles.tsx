73ebb949ca35a64eb4b8527d18e186c4a1d9f039
Tom Jefferys <tomjefferys@gmail.com>
2025-04-07 07:49:31 +0100

    Remove originalSizes ref, store this info in the outerDivs ref.  Remove unused variables and commented out code.

--- src/components/bubbles.tsx
+++ src/components/bubbles.tsx
@@ -33,29 +33,14 @@ const debounce = (func: Function, wait: number) => {
 };
 
 const Bubbles = ({ content } : Content) => {
-    //const [positions, setPositions] = useState<{ [key: string]: { x: number; y: number } }>({});
     const [scrollPosition, setScrollPosition] = useState({ scrollTop: 0, scrollLeft: 0 });
     const [isLoaded, setIsLoaded] = useState(false);
     const [isDragging, setIsDragging] = useState(false);
     const [startMousePosition, setStartMousePosition] = useState({ x: 0, y: 0 });
     const containerRef = useRef<HTMLDivElement>(null);
     const rowRefs = useRef<HTMLDivElement[]>([]);
-    const outerDivs = useRef<HTMLDivElement[][]>([]);
-    const originalSizes = useRef<{ [key: string]: { width: number; height: number } }>({});
+    const outerDivs = useRef<DOMRect[][]>([]);
     const items = useRef<HTMLDivElement[][]>([]);
-
-    //const handleDragStart = (event: React.DragEvent<HTMLDivElement>, item: string) => {
-    //    event.dataTransfer.setData('text/plain', item);
-    //};
-
-    //const handleDragEnd = (event: React.DragEvent<HTMLDivElement>, item: string) => {
-    //    const newPositions = { ...positions };
-    //    newPositions[item] = {
-    //        x: event.clientX - event.currentTarget.offsetWidth / 2,
-    //        y: event.clientY - event.currentTarget.offsetHeight / 2,
-    //    };
-    //    setPositions(newPositions);
-    //};
 
     const handleScroll = () => {
         if (containerRef.current) {
@@ -92,16 +77,12 @@ const Bubbles = ({ content } : Content) => {
         }
     }, []);
 
-    const getRatioFromCentre = (element: HTMLDivElement, row : number, column : number) : Dimension => {
+    const getRatioFromCentre = (row : number, column : number) : Dimension => {
         const container = containerRef.current;
         if (!container) return { x: 0, y: 0 };
         const containerRect = container.getBoundingClientRect();
-        const elementRect = element.getBoundingClientRect();
-        const rowElement = rowRefs.current[row];
-        const rowRect = rowElement.getBoundingClientRect();
 
-        const outerDiv = outerDivs.current[row][column];
-        const outerDivRect = outerDiv.getBoundingClientRect();
+        const outerDivRect = outerDivs.current[row][column];
 
         // Get the centre of the container and the element
         const containerCentre = {
@@ -129,15 +110,13 @@ const Bubbles = ({ content } : Content) => {
     }
 
 
-    const getScale = (element: HTMLDivElement, ratioFromCentre: Dimension) : Scale => {
+    const getScale = (ratioFromCentre: Dimension) : Scale => {
         const container = containerRef.current;
         if (!container) return {verticalScale: 1, horizontalScale: 1};
-        //const containerRect = container.getBoundingClientRect();
-        //const elementRect = element.getBoundingClientRect();
 
         const SHRINK_AREA = 0.5;  // The area where the element will shrink, defined as a fraction the distance from the centre.
 
-        const { x, y } = ratioFromCentre; //getRatioFromCentre(element, row);
+        const { x, y } = ratioFromCentre; 
         let verticalScale = 1;
         if(Math.abs(y) > (1 - SHRINK_AREA)) {
             const dY = Math.abs(y) - (1 - SHRINK_AREA);
@@ -154,55 +133,46 @@ const Bubbles = ({ content } : Content) => {
         return {verticalScale, horizontalScale};
     };
 
-    const getTranslation = (element: HTMLDivElement, centreDelta: Dimension, scale : Scale, item: string) 
+    const getTranslation = (centreDelta: Dimension, scale : Scale, row : number, column : number) 
         : { t : Translation, debugStr : string } => {
-        // TODO do we need to keep originalSizes if we have separate outer divs?
-        const originalSize = originalSizes.current[item];
+
+        if (!outerDivs.current || !outerDivs.current[row] || !outerDivs.current[row][column]) {
+            return { t: { x: 0, y: 0 }, debugStr: "" };
+        }
+
+        const originalRect = outerDivs.current[row][column];
+        //const originalSize = originalSizes.current[item];
 
         // Calculate the translation based on the scale
         const MARGIN_SIZE = 2;
-        const height = originalSize.height + MARGIN_SIZE;
-        const width = originalSize.width + MARGIN_SIZE;
+        const height = originalRect.height + MARGIN_SIZE;
+        const width = originalRect.width + MARGIN_SIZE;
 
         const translateYFraction = 1 - scale.verticalScale;
         const translateYMagnitude = (height * translateYFraction) / 2;
         let y = (Math.sign(centreDelta.y)) * translateYMagnitude;
 
         const translateXFraction = 1 - scale.horizontalScale;
-        const translateXMagnitude = (originalSize.width * translateXFraction) / 2;
+        const translateXMagnitude = (width * translateXFraction) / 2;
         let x = (Math.sign(centreDelta.x)) * translateXMagnitude;
         
         let debugStr = "";
         const container = containerRef.current;
         if (container) {
-            // Caclulate the vertical translation to keep elements in adjacent rows close
+            // Calculate the vertical translation to keep elements in adjacent rows close
             const containerRect = container.getBoundingClientRect();
+
             // Calculate the difference between the spacing of the rows and the optimal spacing.
-            // TODO because the object are ovals, the radius isn't height / 2
-            const radius = (height + width) / 4;
             const hRadius = height / 2;
-            const wRadius = width / 2;
             const optimalVerticalDistance = Math.sqrt(3) * hRadius;
-            //const optimalVerticalDistance = Math.sqrt((4*(radius * radius)) - (wRadius * wRadius))
+
             // Optimal vertical disatance (oval calculation) is coming out at 63 vs 43 for the height
             const verticalAdjustment = height - optimalVerticalDistance;
             if (Math.abs(centreDelta.y) < 2) {
-
-                // This isn't quite right, the distance is too wide, although the scrolling is smooth
                 const distanceFormCentre = centreDelta.y * (containerRect.height/2);
                 const adjustmentRatio = distanceFormCentre / height;
-
                 const dy = adjustmentRatio * verticalAdjustment;
-
                 y += dy;
-
-                //debugStr += `dy: ${dy}, 
-                //                y: ${y}, 
-                //                cd.y: ${centreDelta.y}, 
-                //                adj: ${adjustmentRatio}, 
-                //                dist: ${distanceFormCentre}, 
-                //                opt: ${optimalVerticalDistance}`;
-
             }
         }
 
@@ -260,7 +230,7 @@ const Bubbles = ({ content } : Content) => {
             if (!outerDivs.current[row]) {
                 outerDivs.current[row] = [];
             }
-            outerDivs.current[row][column] = el;
+            outerDivs.current[row][column] = el.getBoundingClientRect();
         }
     }
     , []);
@@ -271,28 +241,9 @@ const Bubbles = ({ content } : Content) => {
                 items.current[row] = [];
             }
             items.current[row][column] = el;
-            if (!originalSizes.current[item]) {
-                originalSizes.current[item] = {
-                    width: el.getBoundingClientRect().width,
-                    height: el.getBoundingClientRect().height,
-                };
-            }
         } }, []);
 
     useEffect(() => {
-        // Apply scaling to each item after the component mounts
-        //rowRefs.current.forEach((row) => {
-        //    if (row) {
-        //        Array.from(row.children).forEach((child) => {
-        //            const el = child as HTMLDivElement;
-        //            const centreDelta = getRatioFromCentre(el);
-        //            const { verticalScale, horizontalScale } = getScale(el, centreDelta);
-        //            const scale = verticalScale * horizontalScale;
-        //            const { x, y } = getTranslation(el, centreDelta, { horizontalScale, verticalScale }, el.dataset.foo || '');
-        //            el.style.transform = `scale(${scale}) translate(${x}px, ${y}px)`;
-        //        });
-        //    }
-        //});
         setIsLoaded(true);
     }, [rows, scrollPosition]);
 
@@ -341,7 +292,6 @@ const Bubbles = ({ content } : Content) => {
                 overflowX: 'auto',
                 overflowY: 'auto',
                 display: 'grid',
-                //padding: '100px 20px 100px 20px',
                 padding: '5vh 15vw',
                 border: '1px solid white',
                 boxSizing: 'border-box',
@@ -354,16 +304,14 @@ const Bubbles = ({ content } : Content) => {
                         display: 'grid', 
                         //gridTemplateColumns: `repeat(${NUM_COLS}, minmax(100px, 1fr))`,
                         gridTemplateColumns: `repeat(${NUM_COLS}, minmax(auto, 100px))`,
-                        //justifyContent: 'center',
-                        marginLeft: rowIndex % 2 === 1 && rowRefs.current[rowIndex]
-                            ? `${originalSizes.current[row[0]]?.width / 2}px`
+                        marginLeft: rowIndex % 2 === 1 && outerDivs.current[rowIndex]
+                            ? `${outerDivs.current[rowIndex][0]?.width / 2}px`
                         : '0',
-                        marginRight: rowIndex % 2 === 0 && rowRefs.current[rowIndex]
-                            ? `${originalSizes.current[row[0]]?.width / 2}px`
+                        marginRight: rowIndex % 2 === 0 && outerDivs.current[rowIndex]
+                            ? `${outerDivs.current[rowIndex][0]?.width / 2}px`
                         : '0',
                         willChange: 'transform',
                         visibility: isLoaded ? 'visible' : 'hidden',
-                        //minWidth: '100%',
                         }}>
 
                     {row.map((item, index) => (
@@ -390,12 +338,11 @@ const Bubbles = ({ content } : Content) => {
                                 ref={(el) => {
                                     setItemRef(el, item, rowIndex, index);
                                     if (el) {
-                                        const centreDelta = getRatioFromCentre(el, rowIndex, index);
-                                        const {verticalScale, horizontalScale} = getScale(el, centreDelta);  
+                                        const centreDelta = getRatioFromCentre(rowIndex, index);
+                                        const {verticalScale, horizontalScale} = getScale(centreDelta);  
                                         const scale = verticalScale * horizontalScale;
-                                        const {t, debugStr} = getTranslation(el, centreDelta, {horizontalScale, verticalScale}, item);
+                                        const {t, debugStr} = getTranslation(centreDelta, {horizontalScale, verticalScale}, rowIndex, index);
                                         const {x, y} = t;
-                                        const boundingRect = el.getBoundingClientRect();
                                         // Translate must come before scale, or the translation amounts will be scaled
                                         el.style.transform = `translateZ(0) translate(${x}px, ${y}px) scale(${scale}) `;
                                         el.style.transformOrigin = 'center';
