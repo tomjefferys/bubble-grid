17eafc642d43dbb6ab726f32f52ec7560da8513c
Tom Jefferys <tomjefferys@gmail.com>
2025-04-28 07:35:49 +0100

    Remove nextjs

--- Makefile
+++ Makefile
@@ -8,8 +8,8 @@ build: node_modules
 	npm run build
 
 test: build
-	npm test
+	npx jest
 
 clean:
 	rm -rf node_modules
-	rm -rf out
+	rm -rf dist
---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
+
+export default config;
